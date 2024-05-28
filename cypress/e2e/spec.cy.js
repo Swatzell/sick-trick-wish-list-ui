636b0cfbfee126f6ef8d59a0790791de62535fec
David Swatzell <thatsstrikethree@gmail.com>
2024-05-28 12:20:25 -0500

    add initial cypress before and intercept and test for api data

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,21 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Trick Form and api trickList', () => {
+  beforeEach(() => {
+    cy.fixture('example.json').as('tricksData');
+    cy.intercept('GET', 'http://localhost:3001/api/v1/tricks', { fixture: 'example.json' }).as('getTricks');
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should display tricks fetched from the API', function() {
+    cy.get('ul').within(() => {
+      cy.get('li').should('have.length', this.tricksData.length);
+      this.tricksData.forEach(trick => {
+        cy.contains(`Stance:${trick.stance}`);
+        cy.contains(`Name:${trick.name}`);
+        cy.contains(`Obstacle:${trick.obstacle}`);
+        cy.contains(`Tutorial: ${trick.tutorial}`);
+      });
+    });
+  });
+
+
+    });
