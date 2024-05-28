b5579b8d6c1f1a328196eb939e37802d69fe52d6
David Swatzell <thatsstrikethree@gmail.com>
2024-05-28 12:23:17 -0500

    add test for input value of form

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -17,5 +17,12 @@ describe('Trick Form and api trickList', () => {
     });
   });
 
+  it('should allow the input value in the form', () => {
+    cy.get('select').first().select('Regular').should('have.value', 'Regular');
+    cy.get('input[placeholder="Name of Trick"]').type('Heelflip').should('have.value', 'Heelflip');
+    cy.get('select').eq(1).select('Rail').should('have.value', 'Rail');
+    cy.get('input[placeholder="Link to Tutorial"]').type('https://example.com/heelflip').should('have.value', 'https://example.com/heelflip');
+  });
+
 
     });
