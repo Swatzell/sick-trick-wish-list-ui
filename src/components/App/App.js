f7acc3b9f114b6a1be3626ae8d49211b17bed602
David Swatzell <thatsstrikethree@gmail.com>
2024-05-28 10:41:10 -0500

    add trick list functon and assign to usestate and add use effect for api call

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,19 @@ import React, { useEffect, useState } from 'react';
 
 
 function App() {
+
+const TrickList = () => {
+  const [tricks, setTricks] = useState([]);
+  
+  useEffect(() => {
+    fetch('http://localhost:3001/api/v1/tricks')
+      .then(response => response.json())
+      .then(data => setTricks(data))
+      .catch(error => console.error('Error fetching tricks:', error));
+  }, []);
+
+
+
   return (
     <div className="App">
       <h1>Sick Trick Wish List</h1>
