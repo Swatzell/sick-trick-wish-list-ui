4440e9461f823cfa81ae66bcf896bd9b84e60791
David Swatzell <thatsstrikethree@gmail.com>
2024-05-28 11:10:48 -0500

    add usestate import for form.js as well as form function

---
+++ src/components/Form/Form.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Form = ({ addTrick }) => {  
+  const [stance, setStance] = useState('');
+  const [name, setName] = useState('');
+  const [obstacle, setObstacle] = useState('');
+  const [tutorial, setTutorial] = useState('');
+
+
+
+
+
+
+}
